044a07bf2e20701ea69c9c20cf1a4df63c90a5cb
Subangkar <subangkar.karmaker@gmail.com>
2020-05-03 11:08:47 +0600

    account-cleanup & urls fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,13 +9,7 @@ urlpatterns = [
 	# path('login/', views.LoginView.as_view(), name='login'),
 	path('login/', views.SignupLoginView.as_view(), name='login'),
 
-	path('manager_login/', views.LoginView.as_view(), name='manger_login'),
-	path('delivery_login/', views.LoginView.as_view(), name='delivery_login'),
-	path('manager_register/', views.LoginView.as_view(), name='manager_register'),
-	path('delivery_register/', views.LoginView.as_view(), name='delivery_register'),
-	path('branch_register/', views.LoginView.as_view(), name='branch_register'),
-
-	path('admin_login/', views.LoginView.as_view(), name='admin_login'),
+	path('admin-login/', views.LoginView.as_view(), name='admin-login'),
 
 	path('register/', views.SignupLoginView.as_view(),name='register'),
 	path(r'register/facebook-signUp/', include('allauth.urls'), name='facebook-signUp'),
@@ -23,9 +17,7 @@ urlpatterns = [
 	path('recovery/', views.LoginView, name='recovery'),
 
 	path('logout/', views.LogoutView.as_view(), name='logout'),
-	path('manager_logout/', views.LoginView.as_view(), name='manager_logout'),
-	path('delivery_logout/', views.LoginView.as_view(), name='delivery_logout'),
 
-	path('admin_logout/', views.LoginView.as_view(), name='admin_logout'),
+	path('admin-logout/', views.LoginView.as_view(), name='admin-logout'),
 
 ]
