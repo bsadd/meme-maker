039380972ea28e60008ad6049c4673c627d44f51
Subangkar <subangkar.karmaker@gmail.com>
2020-05-15 00:41:16 +0600

    google-login-basic
    - google-login with email a/c successful
    - google works with https custom domain
    - google-avatar retrieved

--- initdb.bat
+++ initdb.bat
@@ -2,4 +2,4 @@ bash resetmigrations.sh
 rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata db.json -e contenttypes -e auth -e sessions
+python manage.py loaddata db.json -e contenttypes -e auth -e sessions -e admin
