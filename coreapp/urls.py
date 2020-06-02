e27281049a1ba141c451bbea173ab11304c48b28
Subangkar <subangkar.karmaker@gmail.com>
2020-06-03 03:07:36 +0600

    ManyToMany serialized

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-import views
+from coreapp import views
 
 router = routers.DefaultRouter()
 router.register(r'posts', views.PostViewSet)
--- memesbd/serializers.py
+++ memesbd/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 from memesbd.models import *
@@ -15,31 +14,32 @@ class KeywordSerializer(serializers.ModelSerializer):
 
 
 class PostReactSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    # id = serializers.ReadOnlyField(source='post.id')
+
+    # re = serializers.Field(source='post.name')
+
     class Meta:
         model = PostReact
-        fields = ['react']
+        fields = ['react', 'user']
 
     # def to_representation(self, value):
-    #     return value.name
+    #     return value
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer()
     moderator = UserSerializer()
     keywords = KeywordSerializer(many=True)
-    keywords = serializers.PrimaryKeyRelatedField(queryset=KeywordList.objects.all(), many=True)
 
+    reacts = PostReactSerializer(source='postreact_set', many=True)
     # serializers.PrimaryKeyRelatedField(queryset=KeywordList.objects.all(), many=True, read_only=True)
 
     class Meta:
         model = Post
         fields = ['id', 'caption', 'image', 'nviews', 'is_adult',
                   'is_violent', 'configuration_head', 'configuration_over', 'configuration_tail', 'approval_status',
-                  'approval_details', 'time', 'moderator', 'template', 'author', 'keywords']  # , 'reacts', 'comments'
+                  'approval_details', 'time', 'moderator', 'template', 'author', 'keywords', 'reacts']  # , 'comments'
         # [f.name for f in Post._meta.get_fields()]
         # ['id','caption','image_url','nviews','is_adult','is_adult']
-
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ['url', 'name']
