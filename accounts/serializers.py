58b2eba2d051397086707909f472d5366cd841d8
Subangkar <subangkar.karmaker@gmail.com>
2020-06-12 17:46:34 +0600

    login/register tests

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,8 +5,9 @@ from accounts.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        """TODO: limit fields"""
         model = User
-        fields = ['username', 'email', 'is_superuser', 'is_moderator']  # '__all__'
+        fields = ['username', 'first_name', 'last_name']  # , 'is_superuser', 'is_moderator'  # '__all__'
         read_only_fields = ('is_active', 'is_staff', 'is_superuser')
         extra_kwargs = {
             'password': {'write_only': True}
